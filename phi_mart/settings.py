da65d50f0844a0092bfbc1a3d2f15138ac01d9c4
towfik <mjtowfik659672@gmail.com>
2025-04-19 02:01:05 +0600

    email error

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -194,7 +194,7 @@ DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
